d2ff37de29ad2cefdc893ef1f1e9875c777ff94b
Joshua <76861215+josh-the-o@users.noreply.github.com>
2025-06-18 20:13:07 -0400

    Add files via upload

---
+++ chatgpt project/speech to text/Credentials.h
@@ -0,0 +1,18 @@
+// Provide all the crdentials here
+
+
+
+// Your WiFi Credentials 
+const char *ssid = "Roboprenr-5G";       // WiFi SSID Name
+const char *password = "Robop@123";// WiFi Password ( Keep it blank if your WiFi router is open )
+
+
+// Parameters of Google Speech to Text API
+const String ApiKey = "AIzaSyA6L_397IGYJtHVcUjMmGZXHF8IWRyYAPs";// Google Speech to Text API key
+String SpeechtoText_Language = "en-IN"; // Language 
+
+// Parameters of OpenAI API 
+const char* chatgpt_token = "sk-proj-6985xwCLH9LWmZYdpq6LT3BlbkFJ9XzbsaaDGw1aYxIuOgAt"; // OpenAI API Key
+String OpenAI_Model = "gpt-3.5-turbo-instruct"; // Model
+String OpenAI_Temperature = "0.20"; // temperature
+String OpenAI_Max_Tokens = "40"; //Max Tokens 
